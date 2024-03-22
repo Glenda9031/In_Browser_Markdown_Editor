3e5f46b8847dea48c4e504fc2e2ba04f70212a75
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-03-22 18:09:49 -0500

    Added folders and files

---
+++ src/components/editor/editor.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from "react";
+import { useAppSelector } from "../../hooks/redux";
+import { Markdown } from "../markdown/Markdown";
+import Preview from "../preview/Preview";
+
+export default function Editor(): ReactElement {
+ return (
+    <div className="editor">
+      <Markdown />
+      <Preview />
+    </div>
+  );
+}
+```
